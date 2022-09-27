a9200990ede702f00765aa2b3f307f6c51ca6377
J4r0d-x55 <108584837+J4r0d-x55@users.noreply.github.com>
2022-09-27 10:35:52 +0200

    Update Encrypt.py

--- Encrypt.py
+++ Encrypt.py
@@ -1,16 +1,16 @@
 from hashlib import sha256
 
-entree = input("Entrez le nom du fichier à chiffrer: ")
-sortie = input("Entrez le nom du fichier final: ")
+enter = input("Entrez le nom du fichier à chiffrer: ")
+output = input("Entrez le nom du fichier final: ")
 key = input(("Entrez la clé: "))
 keys = sha256(key.encode('utf-8')).digest()
 
-with open(entree, 'rb') as f_entree:
-    with open(sortie, 'wb') as f_sortie:
+with open(enter, 'rb') as f_enter:
+    with open(output, 'wb') as f_output:
         i = 0
-        while f_entree.peek():
-            c = ord(f_entree.read(1))
+        while f_enter.peek():
+            c = ord(f_enter.read(1))
             j = i % len(keys)
             b = bytes([c ^ keys[j]])
-            f_sortie.write(b)
+            f_output.write(b)
             i = i + 1
